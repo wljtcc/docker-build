66c7175b410194cea3a1ed6e5096c9926ba27d10
Wellington <wljtcc@gmail.com>
2018-05-03 13:59:22 -0300

    Cria shell para instalar docker/docker-compose/docker-cleanup

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Install Git"
+apt install git -y
+
+echo "Install Docker - Latest Version"
+wget -qO- https://get.docker.com/ | sh
+
+echo "Install DockerCompose - Latest Version"
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+echo "Install Docker Cleanup - Clean Trash Docker"
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
