d3489a38e6c0a3b6ff9d815b5c20c89a19dea1b5
Wellington <wljtcc@gmail.com>
2020-01-23 10:37:23 -0300

    Atualiza a instalação do docker para o Redhat e centos 8

--- install_docker.sh
+++ install_docker.sh
@@ -18,3 +18,18 @@ git clone https://gist.github.com/76b450a0c986e576e98b.git
 cd 76b450a0c986e576e98b
 sudo mv docker-cleanup /usr/local/bin/docker-cleanup
 sudo chmod +x /usr/local/bin/docker-cleanup
+
+# RedHat8 / Cenots8
+
+UNAME=`uname -a | awk '{print $2}'`
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf clean packages
+yum update
+
+yum remove podman
+dnf install docker-ce --nobest -y
+
+systemctl start docker
+systemctl enable docker
+docker ps -a
