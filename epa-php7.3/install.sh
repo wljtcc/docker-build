620c16112576c14c72679ea4e7cef91e9a6a44f5
Wellington Luiz <wljtcc@gmail.com>
2022-04-18 10:45:51 -0300

    Atualiza php do epa para 7.3.3-1

--- epa-php7.3/install.sh
+++ epa-php7.3/install.sh
@@ -108,7 +108,10 @@ rm -rf /var/lib/apt/lists/*
 # Set permission run.sh
 chmod 0777 /etc/init.d/run.sh
 
+update-alternatives --set php /usr/bin/php7.3
+
 rm -rf /var/spool/cron/crontabs/root
-echo "* * * * * php \$EPA_PATH/api/artisan schedule:run &> /dev/null" > /var/spool/cron/crontabs/root
+# echo "* * * * * php \$EPA_PATH/api/artisan schedule:run &> /dev/null" > /var/spool/cron/crontabs/root
+echo "" > /var/spool/cron/crontabs/root
 chown root:crontab /var/spool/cron/crontabs/root
 chmod 0600 /var/spool/cron/crontabs/root
