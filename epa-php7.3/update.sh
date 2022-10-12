7d508802e4762ca19f23ec718cd0e3076b5398dc
Wellington Luiz <wljtcc@gmail.com>
2022-10-12 16:07:46 -0300

    Atualização

--- epa-php7.3/update.sh
+++ epa-php7.3/update.sh
@@ -7,15 +7,18 @@ apt -y install wget
 echo "Upgrade"
 apt -y upgrade
 
+# Set permission run.sh
+chmod 0777 /etc/init.d/run.sh
+
+update-alternatives --set php /usr/bin/php7.3
+
+rm -rf /var/spool/cron/crontabs/root
+# echo "* * * * * php \$EPA_PATH/api/artisan schedule:run &> /dev/null" > /var/spool/cron/crontabs/root
+echo "" > /var/spool/cron/crontabs/root
+chown root:crontab /var/spool/cron/crontabs/root
+chmod 0600 /var/spool/cron/crontabs/root
+
 echo "Remove Cache APT"
 # Removendo cache
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
-
-# Set permission run.sh
-chmod 0777 /etc/init.d/run.sh
-
-touch /var/spool/cron/crontabs/root
-echo "* * * * * php \$EPA_PATH/api/artisan schedule:run &> /dev/null" >> /var/spool/cron/crontabs/root
-chown root:crontab /var/spool/cron/crontabs/root
-chmod 0600 /var/spool/cron/crontabs/root
